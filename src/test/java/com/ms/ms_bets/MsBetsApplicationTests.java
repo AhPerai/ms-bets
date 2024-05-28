5989d22047c325cfd8f99ad9473a779b32c2a0b4
Gabriel Perini <10140827900@edu.udesc.br>
2024-05-28 16:45:25 -0300

    added spring initializr project

---
+++ src/test/java/com/ms/ms_bets/MsBetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.ms_bets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
